c264b18e667bb338125b51691268b44caacceb1b
Sean Cresswell <sean.cresswell@digital.landregistry.gov.uk>
2017-11-16 15:19:38 +0000

    make changes

--- src/test/java/com/mysite/selenium/ParkingCalculatorCalculation.java
+++ src/test/java/com/mysite/selenium/ParkingCalculatorCalculation.java
@@ -14,16 +14,13 @@ public class ParkingCalculatorCalculation extends ParkingCalculator{
         driver.findElement(By.id("EntryDate")).clear();
         driver.findElement(By.id("EntryDate")).sendKeys("01/01/2016");
 
-        driver.findElement(By.id("ExitTime")).clear();
-        driver.findElement(By.id("ExitTime")).sendKeys("7:00");
+        //more stuff goes here
 
-        driver.findElement(By.id("ExitDate")).clear();
-        driver.findElement(By.id("ExitDate")).sendKeys("01/01/2016");
 
+        //
         driver.findElement(By.name("Submit")).click();
 
         String CalculatedValue = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/span[1]/font/b")).getText();
-
         Assert.assertEquals("String doesn't match!",CalculatedValue,"$ 2.00");
     }
 }
